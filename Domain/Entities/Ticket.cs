edaafdcd838e6376e3e133b8e34dc1971ea83189
Genes Luna <genesluna@gmail.com>
2022-11-02 13:29:54 -0300

    removed date property

--- Domain/Entities/Ticket.cs
+++ Domain/Entities/Ticket.cs
@@ -7,7 +7,6 @@ public enum TicketType
 
 public class Ticket : BaseEntity
 {
-  public DateTimeOffset Date { get; set; }
   public TicketType TicketType { get; set; }
   public decimal Price { get; private set; }
   public Guid SessionId { get; set; }
