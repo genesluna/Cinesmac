8367f0ea403adcb123b8b011d1b41b37814916d7
Genes Luna <genesluna@gmail.com>
2022-11-05 13:50:12 -0300

    added shop module

--- Client/src/app/shop/shop.component.ts
+++ Client/src/app/shop/shop.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../shared/models/movie';
+import { ShopService } from './shop.service';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
+  movies: Movie[];
 
-  constructor() { }
+  constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
+    this.shopService.getMovies().subscribe(
+      (response) => {
+        this.movies = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
--- Client/src/app/shop/shop.service.ts
+++ Client/src/app/shop/shop.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Movie } from '../shared/models/movie';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/v1/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.baseUrl + 'movies');
+  }
 }
