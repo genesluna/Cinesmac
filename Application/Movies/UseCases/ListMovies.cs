579f0fada3f2c31c7f24b41686c39158d92bca8b
Genes Luna <genesluna@gmail.com>
2022-11-04 21:12:22 -0300

    added imapper projection and pagination

--- Application/Movies/UseCases/ListMovies.cs
+++ Application/Movies/UseCases/ListMovies.cs
@@ -1,6 +1,7 @@
 using Application.Core;
 using Application.Movies.Dtos;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -9,9 +10,12 @@ namespace Application.Movies.UseCases;
 
 public class ListMovies
 {
-  public class Query : IRequest<Result<IReadOnlyList<MovieDto>>> { }
+  public class Query : IRequest<Result<PagedList<MovieDto>>>
+  {
+    public PagingParameters PagingParams { get; set; }
+  }
 
-  public class Handler : IRequestHandler<Query, Result<IReadOnlyList<MovieDto>>>
+  public class Handler : IRequestHandler<Query, Result<PagedList<MovieDto>>>
   {
     private readonly DataContext _context;
     private readonly IMapper _mapper;
@@ -21,16 +25,14 @@ public class ListMovies
       _context = context;
     }
 
-    public async Task<Result<IReadOnlyList<MovieDto>>> Handle(Query request, CancellationToken cancellationToken)
+    public async Task<Result<PagedList<MovieDto>>> Handle(Query request, CancellationToken cancellationToken)
     {
-      var movies = await _context.Movies
-              .Include(m => m.Sessions)
-              .ThenInclude(s => s.ScreeningRoom)
-              .ToListAsync(cancellationToken);
+      var query = _context.Movies.ProjectTo<MovieDto>(_mapper.ConfigurationProvider).AsQueryable();
 
-      var moviesDto = _mapper.Map<IReadOnlyList<MovieDto>>(movies);
+      var moviesDto = await PagedList<MovieDto>.CreateAsync(query, request.PagingParams.PageNumber,
+              request.PagingParams.PageSize, cancellationToken);
 
-      return Result<IReadOnlyList<MovieDto>>.Success(moviesDto);
+      return Result<PagedList<MovieDto>>.Success(moviesDto);
     }
   }
 }
