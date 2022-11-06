e20113a39417fa5b89ebd48a756600358d8f2099
Genes Luna <genesluna@gmail.com>
2022-11-06 11:13:35 -0300

    added pagination

--- Client/src/app/shop/shop.module.ts
+++ Client/src/app/shop/shop.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { MovieItemComponent } from './movie-item/movie-item.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ShopComponent, MovieItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
