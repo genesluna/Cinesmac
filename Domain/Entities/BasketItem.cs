7eb263e839512bd2481ebb642bdfa6f24b23ad4f
Genes Luna <genesluna@gmail.com>
2022-11-07 18:03:59 -0300

    refactoring

--- Domain/Entities/BasketItem.cs
+++ Domain/Entities/BasketItem.cs
@@ -1,10 +1,12 @@
+using Domain.Enums;
+
 namespace Domain.Entities;
 
 public class BasketItem
 {
-  public int Id { get; set; }
-  public Guid SessionID { get; set; }
+  public string Id { get; set; }
   public int SessionTime { get; set; }
+  public TicketType TicketType { get; set; }
   public string MovieTitle { get; set; }
   public string ScreeningRoomName { get; set; }
   public decimal Price { get; set; }
