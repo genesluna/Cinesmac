62b1d27b9ead42a526aea95932bc1121e5a8f35d
Genes Luna <genesluna@gmail.com>
2022-11-07 18:03:35 -0300

    updated rules

--- Application/Baskets/Validators/BasketItemValidator.cs
+++ Application/Baskets/Validators/BasketItemValidator.cs
@@ -8,8 +8,8 @@ public class BasketItemValidator : AbstractValidator<BasketItem>
   public BasketItemValidator()
   {
     RuleFor(x => x.Id).NotEmpty();
-    RuleFor(x => x.SessionID).NotEmpty();
     RuleFor(x => x.SessionTime).NotEmpty();
+    RuleFor(x => x.TicketType).NotEmpty();
     RuleFor(x => x.MovieTitle).NotEmpty();
     RuleFor(x => x.ScreeningRoomName).NotEmpty();
     RuleFor(x => x.Price).NotEmpty();
