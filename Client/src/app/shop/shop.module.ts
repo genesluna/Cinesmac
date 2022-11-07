f3a6b1aba31c24edaed551359dc259512ee266cb
Genes Luna <genesluna@gmail.com>
2022-11-06 23:54:24 -0300

    added routing

--- Client/src/app/shop/shop.module.ts
+++ Client/src/app/shop/shop.module.ts
@@ -1,12 +1,14 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { MovieItemComponent } from './movie-item/movie-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
-  declarations: [ShopComponent, MovieItemComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [ShopComponent, MovieItemComponent, MovieDetailsComponent],
+  imports: [CommonModule, SharedModule, RouterModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
