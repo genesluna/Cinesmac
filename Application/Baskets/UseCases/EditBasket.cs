1cd8a4e99c2aef3c1566b03355cb24a6a4947ff8
Genes Luna <genesluna@gmail.com>
2022-11-07 23:34:24 -0300

    now returns basket back

--- Application/Baskets/UseCases/EditBasket.cs
+++ Application/Baskets/UseCases/EditBasket.cs
@@ -8,12 +8,12 @@ namespace Application.Baskets.UseCases;
 
 public class EditBasket
 {
-  public class Command : IRequest<Result<Unit>>
+  public class Command : IRequest<Result<Basket>>
   {
     public Basket Basket { get; set; }
   }
 
-  public class Handler : IRequestHandler<Command, Result<Unit>>
+  public class Handler : IRequestHandler<Command, Result<Basket>>
   {
     private readonly IDatabase _db;
     public Handler(IConnectionMultiplexer redis)
@@ -21,15 +21,17 @@ public class EditBasket
       _db = redis.GetDatabase();
     }
 
-    public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+    public async Task<Result<Basket>> Handle(Command request, CancellationToken cancellationToken)
     {
       var result = await _db.StringSetAsync(request.Basket.Id,
           JsonSerializer.Serialize(request.Basket), TimeSpan.FromDays(7));
 
       if (!result)
-        return Result<Unit>.Failure(ErrorType.SaveChangesError, "Failed to edit basket");
+        return Result<Basket>.Failure(ErrorType.SaveChangesError, "Failed to edit basket");
 
-      return Result<Unit>.Success(Unit.Value);
+      var basket = JsonSerializer.Deserialize<Basket>(await _db.StringGetAsync(request.Basket.Id));
+
+      return Result<Basket>.Success(basket);
 
     }
   }
