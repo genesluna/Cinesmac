bd672dd1edf8de85f7a92f458a3504c016dd9bf3
Genes Luna <genesluna@gmail.com>
2022-11-09 19:43:18 -0300

    added user dtos

---
+++ Application/Users/Dtos/AddressDto.cs
@@ -0,0 +1,9 @@
+namespace Application.Users.Dtos;
+
+public class AddressDto
+{
+  public string Street { get; set; }
+  public string City { get; set; }
+  public string State { get; set; }
+  public string ZipCode { get; set; }
+}
---
+++ Application/Users/Dtos/UserDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Users.Dtos;
+
+public class UserDto
+{
+  public UserDto()
+  {
+  }
+
+  public UserDto(string email, string name, string token)
+  {
+    Email = email;
+    Name = name;
+    Token = token;
+  }
+
+  public string Email { get; set; }
+  public string Name { get; set; }
+  public string Token { get; set; }
+}
