23ec1c61b583e3780b63e697c2bfa57678af7841
Genes Luna <genesluna@gmail.com>
2022-11-09 19:46:45 -0300

    added address mapping

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,8 +1,10 @@
 using Application.Movies.Dtos;
 using Application.ScreeningRooms.Dtos;
 using Application.Sessions.Dtos;
+using Application.Users.Dtos;
 using AutoMapper;
 using Domain.Entities;
+using Domain.Entities.Identity;
 
 namespace Application.Core;
 
@@ -10,6 +12,7 @@ public class MappingProfiles : Profile
 {
   public MappingProfiles()
   {
+    CreateMap<Address, AddressDto>().ReverseMap();
     CreateMap<Movie, MovieDto>();
     CreateMap<MovieCreateDto, Movie>();
     CreateMap<MovieEditDto, Movie>();
