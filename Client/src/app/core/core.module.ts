11c08bca052f3a90ddffbeb973aedd3f30973784
Genes Luna <genesluna@gmail.com>
2022-11-10 19:20:47 -0300

    added import to shared module

--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -5,12 +5,14 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
     RouterModule,
+    SharedModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true,
