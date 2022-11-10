0fe851e4704f480908b8b8c45466d4be752eb8c0
Genes Luna <genesluna@gmail.com>
2022-11-10 19:24:10 -0300

    added reactive forms and bs dropdown

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, PaginationModule.forRoot()],
-  exports: [PaginationModule],
+  declarations: [TextInputComponent],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ReactiveFormsModule,
+  ],
+  exports: [
+    PaginationModule,
+    ReactiveFormsModule,
+    BsDropdownModule,
+    TextInputComponent,
+  ],
 })
 export class SharedModule {}
