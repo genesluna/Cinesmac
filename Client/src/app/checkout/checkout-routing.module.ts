ea0553f63d07e15531850d5c78bd432567f8a2a9
Genes Luna <genesluna@gmail.com>
2022-11-12 15:34:01 -0300

    removed import

--- Client/src/app/checkout/checkout-routing.module.ts
+++ Client/src/app/checkout/checkout-routing.module.ts
@@ -1,6 +1,5 @@
 import { CheckoutComponent } from './checkout.component';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{ path: '', component: CheckoutComponent }];
